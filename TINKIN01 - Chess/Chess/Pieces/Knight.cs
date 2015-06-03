96c32686a3a680cd1d090a04d5e7aa9bbe3ab92b
LarsBroeders <lars_broeders@hotmail.com>
2015-06-03 11:32:39 +0200

    Validate Knight

--- TINKIN01 - Chess/Chess/Pieces/Knight.cs
+++ TINKIN01 - Chess/Chess/Pieces/Knight.cs
@@ -10,17 +10,43 @@ namespace TINKIN01.Chess.Pieces
     {
         public override HashSet<Move> GetValidMoves(Chessboard board)
         {
-            return null; /*
-            return new Point[] { 
-                new Point(1, 2),
-                new Point(1, -2),
-                new Point(-1, 2),
-                new Point(-1, -2),
-                new Point(2, 1),
-                new Point(2, -1),
-                new Point(-2, 1),
-                new Point(-2, -1)
-            };*/
+            var start = board.IndexOf(this);
+            var moves = new HashSet<Move>();
+            Point end;
+
+            end = new Point(start.X + 1, start.Y + 2);
+            if (board.IsValidField(end, Owner) || board.IsValidField(end, Owner, false))
+                moves.Add(new Move(start, end, this));
+
+            end = new Point(start.X + 1, start.Y - 2);
+            if (board.IsValidField(end, Owner) || board.IsValidField(end, Owner, false))
+                moves.Add(new Move(start, end, this));
+
+            end = new Point(start.X - 1, start.Y + 2);
+            if (board.IsValidField(end, Owner) || board.IsValidField(end, Owner, false))
+                moves.Add(new Move(start, end, this));
+
+            end = new Point(start.X - 1, start.Y - 2);
+            if (board.IsValidField(end, Owner) || board.IsValidField(end, Owner, false))
+                moves.Add(new Move(start, end, this));
+
+            end = new Point(start.X + 2, start.Y + 1);
+            if (board.IsValidField(end, Owner) || board.IsValidField(end, Owner, false))
+                moves.Add(new Move(start, end, this));
+
+            end = new Point(start.X + 2, start.Y - 1);
+            if (board.IsValidField(end, Owner) || board.IsValidField(end, Owner, false))
+                moves.Add(new Move(start, end, this));
+
+            end = new Point(start.X - 2, start.Y + 1);
+            if (board.IsValidField(end, Owner) || board.IsValidField(end, Owner, false))
+                moves.Add(new Move(start, end, this));
+
+            end = new Point(start.X - 2, start.Y - 1);
+            if (board.IsValidField(end, Owner) || board.IsValidField(end, Owner, false))
+                moves.Add(new Move(start, end, this));
+
+            return moves;
         }
     }
 }

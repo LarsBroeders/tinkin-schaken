82351245245d409e89397188b6b931cdb0b5b6e2
Koen Klever <boarderkoen@hotmail.com>
2015-06-10 08:55:05 +0200

    Correct White King/Queen start position

--- TINKIN01 - Chess/Form1.cs
+++ TINKIN01 - Chess/Form1.cs
@@ -30,7 +30,7 @@ namespace TINKIN01
         public Form1()
         {
             Player1 = new Human(TeamEnum.White, GetMoveAsync);
-            Player2 = new Human(TeamEnum.Black, GetMoveAsync);
+            Player2 = new ChessAI(TeamEnum.Black);
             InitializeComponent();
 
             var pieces = chessComponent1.Board.Pieces.ToIEnumerable();
